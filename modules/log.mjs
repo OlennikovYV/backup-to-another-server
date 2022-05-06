f3ba142e1e226b618768d789145a66dd9f6c267e
Yury <olennikovyv@mail.ru>
2022-05-06 16:38:48 +0500

    add 'TODO add error checking' in Log Class

--- modules/log.mjs
+++ modules/log.mjs
@@ -2,6 +2,7 @@ import fs from 'fs'
 
 class Log {
     constructor(file) {
+        // TODO add error checking
         this._stream = fs.createWriteStream(file, { flags: 'a' });
     }
 
@@ -9,9 +10,12 @@ class Log {
         const date = new Date().toISOString().split('T');
         return date[0] + ' ' + date[1].split('.')[0];
     }
+
     logs(text, type = 'Info') {
         this._stream.write(`${this.getDate()} - [${type}]: ${text}\n`);
     }
+
+    // TODO add error checking
     closeStream() { this._stream.end() }
 }
 
